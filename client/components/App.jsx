c2c5483ff742f02d83d9e4876e81a9bf43826133
Martin Starosta <martin@mayorsoft.eu>
2017-04-20 00:12:20 +0200

    Initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,11 @@
+/*
+    ./client/components/App.jsx
+*/
+import React from 'react';
+
+const App = (props)=>
+    <div style={{textAlign: 'center'}}>
+        <h1>Hello from React Stateless Component</h1>
+    </div>;
+
+export default App;
